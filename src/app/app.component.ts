2d22998772f912e936755de21f074fbf5f49ff6e
Seokweon Jung <minimus@snu.ac.kr>
2019-03-29 17:15:57 +0900

    leaflet pracice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css' ]
 })
-export class AppComponent {
-  title = 'angular-leaflet';
+export class AppComponent implements OnInit {
+  zoom: number;
+  center: L.LatLng;
+  fitBounds: L.LatLngBounds;
+  baseLayers: L.TileLayer[];
+  constructor() {
+
+  }
+
+  ngOnInit() {
+    const map = L.map('map').setView([51.505, -0.09], 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+}
+
 }
