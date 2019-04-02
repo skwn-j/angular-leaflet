8c282f7c73311175453dc918c6522445cac1a479
Seokweon Jung <minimus@snu.ac.kr>
2019-04-02 20:12:12 +0900

    no fs

--- src/app/show-rental-place.service.ts
+++ src/app/show-rental-place.service.ts
@@ -2,13 +2,30 @@ import { Injectable } from '@angular/core';
 import * as L from 'leaflet';
 import 'pixi.js';
 import 'leaflet-pixi-overlay';
+import 'fs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShowRentalPlaceService {
   loader: PIXI.loaders.Loader;
+  text: string;
   constructor() { }
+  openRentalPlace() {
+    const csv = require('csv-parser');
+    const fs = require('fs');
+    const results = [];
+
+    fs.createReadStream('assets/seoul_bicycle/o1_rental_place/rental_place.csvs')
+      .pipe(csv())
+      .on('data', (data) => results.push(data))
+      .on('end', () => {
+        console.log(results);
+      });
+  }
+
+
+
   showRentalPlace(map: L.Map) {
     console.log('rental');
     this.loader = new PIXI.loaders.Loader();
@@ -31,7 +48,7 @@ export class ShowRentalPlaceService {
         let renderer = utils.getRenderer();
         let project = utils.latLngToLayerPoint;
         let scale = utils.getScale();
-
+        scale = scale * 10;
         if (firstDraw) {
           let markerCoords = project(markerLatLng);
           marker.x = markerCoords.x;
